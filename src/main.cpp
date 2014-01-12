2fe7373ee366e771e84afe7b9a1cbcf291a9184e
Boerje <boerje.santen@freenet,de>
2014-01-12 19:06:05 +0100

    Bugfix - Müll löschen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
-
-#include "CaptureVideo.h"
 #include "backgroundsubtraction.h"
 using namespace cv;
 using namespace std;
 
 int main(){
 	
-	CaptureVideo captureVideo;
+
 	BackgroundSubtraction bgs;
 
-		//captureVideo.captureV();
 		bgs.backgroundSubtraction();
 }

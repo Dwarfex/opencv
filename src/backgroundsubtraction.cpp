1058478783f56b921eb615b6ac28fe49adcd4e2a
HenrikOelze <h.oelze@wemake-it.de>
2014-01-12 17:36:04 +0100

    Schwerpunkt
    Schwerpunkt anhand der rein roten Pixel ausfindeig machen und
    einzeichnen

--- src/backgroundsubtraction.cpp
+++ src/backgroundsubtraction.cpp
@@ -17,7 +17,9 @@ void BackgroundSubtraction::backgroundSubtraction(){
 	Mat frame; //Kamera Input, unangetstest
 	Mat front; //Fordegrund (Hand)
 	Mat back; //Hintegrunde (der Rest)
-
+	int middleX = 0;
+	int middleY = 0;
+	int crosssize = 10;
 
     VideoCapture videoCapture;
 	videoCapture.open(0);
@@ -96,6 +98,43 @@ void BackgroundSubtraction::backgroundSubtraction(){
                                                 }
 										}
 								}*/
+
+					int redPixelCount = 0;
+					int redPixelx = 0;
+					int redPixely = 0;
+					
+					for(int x = 0; x < frame.cols; x++){
+						
+						for(int y = 0; y < frame.rows; y++){
+							//uchar pixel = frame.at<uchar>(y,x);
+							
+							Vec3b colour = frame.at<Vec3b>(Point(x, y));
+							if(colour.val[0]==0 && colour.val[1]==0 && colour.val[2]==255){
+							
+							//if(pixel == 255){
+								
+								redPixelCount++;
+								redPixely = redPixely + y;
+								redPixelx = redPixelx + x;
+							}
+
+								
+
+						}
+					}
+					if(redPixelCount > 0){
+					middleX = redPixelx / redPixelCount;
+					middleY = redPixely/ redPixelCount;
+					}
+					 //ende schwerpunkt
+
+					 // Kreuz zeichnen
+						// cvtColor(greyscale, greyscale, CV_GRAY2BGR);
+						Point center(middleX, middleY);
+						line(frame,Point(center.x - crosssize, center.y),Point(center.x + crosssize, center.y),Scalar(0,255,0),2,8);
+						line(frame,Point(center.x, center.y - crosssize),Point(center.x, center.y + crosssize),Scalar(0,255,0),2,8);
+
+					 //ende Kreuz zeichnen
 			}
 
 		}
